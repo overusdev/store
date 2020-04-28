a0a205e1ffd21fb604cec52be3bd84d32314fc72
Сергей Вилков <you@example.com>
2020-04-28 10:03:44 +0300

    Minor

--- src/js/modules/addToCart.js
+++ src/js/modules/addToCart.js
@@ -3,7 +3,7 @@ const addToCart = (function () {
 
     for (let i = 0; i < productItem.length; i += 1) {
         productItem[i].addEventListener('click', () => {
-            productItem[i].classList.toggle("b-product-item_state_active");
+            productItem[i].classList.toggle('b-product-item_state_active');
         });
     }
 
